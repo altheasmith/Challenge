c7be0fafd1a1d62545886ed400bbcbcca217a5f1
Althea Smith <althea.o.smith@gmail.com>
2016-02-09 23:27:46 -0500

    code swap for states complete

--- csv/2_code_swap.py
+++ csv/2_code_swap.py
@@ -9,10 +9,20 @@ import csv
 
 initial_file = open('test.csv', 'rb')
 csv_reader = csv.DictReader(initial_file)
-fieldnames = next(csv_reader).keys()
+state_file = open('state_abbreviations.csv', 'rb')
+state_reader = csv.DictReader(state_file)
+state_names = [row['state_name'] for row in state_reader]
+# Re-declaring state_reader for second list comprehension
+state_file = open('state_abbreviations.csv', 'rb')
+state_reader = csv.DictReader(state_file)
+state_abbrs = [row['state_abbr'] for row in state_reader]
 solution_file = open('solution.csv', 'wb')
+fieldnames = next(csv_reader).keys()
 csv_writer = csv.DictWriter(solution_file, fieldnames=fieldnames)
 csv_writer.writeheader()
 for row in csv_reader:
-    row['bio'] = " ".join(row['bio'].split())
+    row['state'] = state_names[state_abbrs.index(row['state'])]
     csv_writer.writerow(row)
+
+# Try with one for loop
+# Try with 'vlookup' command
---
+++ csv/3_date_offset.py
@@ -0,0 +1,9 @@
+# Challenge:
+
+# Date offset (bonus) - The start_date field contains data in a variety of
+# formats. These may include e.g., "June 23, 1912" or "5/11/1930" (month, day,
+# year). But not all values are valid dates. Invalid dates may include e.g.,
+# "June 2018", "3/06" (incomplete dates) or even arbitrary natural language.
+# Add a start_date_description field adjacent to the start_date column to
+# filter invalid date values into. Normalize all valid date values in
+# start_date to ISO 8601 (i.e., YYYY-MM-DD).
